fb3efb1c81435b868fbfa5185ed6c9f3e86cc4fd
feiben <feiben365@gmail.com>
2017-01-10 18:02:59 +0800

    step6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,10 +17,9 @@ class JobsController < ApplicationController
             when 'by_upper_bound'
               Job.published.order('wage_upper_bound DESC')
             else
-              Job.published.order('created_at DESC')
+              Job.published.recent
             end
   end
-
 
   def new
     @job = Job.new
